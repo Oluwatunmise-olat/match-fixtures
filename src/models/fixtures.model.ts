e324d7ab1df1a6726a911bfc8a3758b2ea1a0752
Oluwatunmise-olat <oluwatunmiseolatunbosun2001@gmail.com>
2024-06-29 17:03:03 +0100

    chore: add build spec yml file

--- src/models/fixtures.model.ts
+++ src/models/fixtures.model.ts
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
-import { container } from "tsyringe";
+import mongoose from 'mongoose'
+import { container } from 'tsyringe'
 
-import FixturesSchema from "./schema/fixtures.schema";
-import { ModelNames } from "@app/shared/enums/models.enum";
+import FixturesSchema from './schema/fixtures.schema'
+import { ModelNames } from '@app/shared/enums/models.enum'
 
-const fixturesModelSchema = container.resolve(FixturesSchema);
+const fixturesModelSchema = container.resolve(FixturesSchema)
 
-export default mongoose.model(ModelNames.FIXTURES, fixturesModelSchema.schema);
+export default mongoose.model(ModelNames.FIXTURES, fixturesModelSchema.schema)
