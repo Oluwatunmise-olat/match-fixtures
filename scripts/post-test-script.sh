822acc40d4f8d24ab05ce2ca11af444791962a1f
Oluwatunmise-olat <oluwatunmiseolatunbosun2001@gmail.com>
2024-06-30 03:17:24 +0100

    chore: enable privilege mode for ec2 engine

--- scripts/post-test-script.sh
+++ scripts/post-test-script.sh
@@ -2,7 +2,7 @@
 
 chmod +x "$0"
 
-rm ./.env
+rm ./.env 2> /dev/null
 
 docker stop $(docker ps -aqf "name=test-mongodb") &> /dev/null && docker rm $(docker ps -aqf "name=test-mongodb") &> /dev/null
 docker stop $(docker ps -aqf "name=test-redis") &> /dev/null && docker rm $(docker ps -aqf "name=test-redis") &> /dev/null
